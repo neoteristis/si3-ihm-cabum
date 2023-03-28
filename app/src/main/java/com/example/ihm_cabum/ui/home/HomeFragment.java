feed76036ae44e2305786daa10881e9fc7f2d157
BadWolf03 <tspam765@gmail.com>
2023-03-28 20:06:29 +0300

    added fragment for google map

--- app/src/main/java/com/example/ihm_cabum/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/ihm_cabum/ui/home/HomeFragment.java
@@ -4,28 +4,30 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.ihm_cabum.R;
 import com.example.ihm_cabum.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
 
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         HomeViewModel homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        Fragment fragment = new MapsFragment();
+
+        getChildFragmentManager().beginTransaction().replace(R.id.map, fragment).commit();
+        View root = binding.getRoot();
         return root;
     }
 
