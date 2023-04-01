0de33d29ccbab3e786ee3e68f3dca2b9cecb86ab
Nina Boulton <ninaboulton@icloud.com>
2023-04-01 14:32:21 +0200

    profile page

--- app/src/main/java/com/example/ihm_cabum/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/ihm_cabum/ui/profile/ProfileFragment.java
@@ -1,32 +1,49 @@
 package com.example.ihm_cabum.ui.profile;
 
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.preference.ListPreference;
+import androidx.preference.MultiSelectListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
 
+import com.example.ihm_cabum.R;
 import com.example.ihm_cabum.databinding.FragmentProfileBinding;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class ProfileFragment extends Fragment {
 
     private FragmentProfileBinding binding;
+    private static View rootView;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        ProfileViewModel profileViewModel =
-                new ViewModelProvider(this).get(ProfileViewModel.class);
-
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        
         binding = FragmentProfileBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        rootView = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        profileViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        getChildFragmentManager()
+                .beginTransaction()
+                .replace(R.id.profile_page, new ProfileInnerFragment())
+                .commit();
+
+        return rootView;
     }
 
     @Override
@@ -34,4 +51,44 @@ public class ProfileFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+    public static class ProfileInnerFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.profile_frame, rootKey);
+
+            MultiSelectListPreference transportPreference = findPreference("transports");
+            transportPreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object value) {
+
+
+                    List<Integer> icons = List.of(R.id.icon1, R.id.icon2, R.id.icon3, R.id.icon4);
+                    Map<String, Integer> transport_map = Map.of(
+                            "Car", R.drawable.ic_car,
+                            "Motorbike", R.drawable.ic_motorbike,
+                            "Bicycle", R.drawable.ic_bicycle,
+                            "Walk", R.drawable.ic_walk);
+
+                    // Remove all icons
+                    for (int icon : icons){
+                        ImageView view = rootView.findViewById(icon);
+                        view.setImageResource(0);
+                    }
+
+                    // Replace with new icons for selected transports
+                    Set<String> transports = (Set<String>) value;
+                    int i=0;
+                    for (String transport : transports){
+                        ImageView view = rootView.findViewById(icons.get(i));
+                        view.setImageResource(transport_map.get(transport));
+                        i++;
+                    }
+                    return true;
+                }
+            });
+
+        }
+    }
+
 }
