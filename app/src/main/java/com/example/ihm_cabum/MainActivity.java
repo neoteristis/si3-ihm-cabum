fc757c002f264d47a33f9860a6bf83d6ab695bb7
BadWolf03 <tspam765@gmail.com>
2023-04-02 04:05:36 +0300

    working open street map (on physical device)

--- app/src/main/java/com/example/ihm_cabum/MainActivity.java
+++ app/src/main/java/com/example/ihm_cabum/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.os.StrictMode;
 
 import com.example.ihm_cabum.databinding.ActivityMainBinding;
 
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
--- app/src/main/java/com/example/ihm_cabum/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/ihm_cabum/ui/home/HomeFragment.java
@@ -1,39 +1,68 @@
 package com.example.ihm_cabum.ui.home;
 
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.Marker;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-
 import com.example.ihm_cabum.R;
-import com.example.ihm_cabum.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
-    private FragmentHomeBinding binding;
+    private MapView mapView;
+    private SharedPreferences sharedPreferences;
+    private Marker marker;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        // Initialize map view and map controller
+        mapView = root.findViewById(R.id.map_view);
+        Configuration.getInstance().load(getContext(), PreferenceManager.getDefaultSharedPreferences(getContext()));
+        mapView.setTileSource(TileSourceFactory.MAPNIK);
+        mapView.setBuiltInZoomControls(true);
+        mapView.setMultiTouchControls(true);
+        mapView.getController().setZoom(10.0);
 
-        Fragment fragment = new MapsFragment();
+        // Set default location and add marker
+        GeoPoint startPoint = new GeoPoint(43.65020, 7.00517);
+        mapView.getController().setCenter(startPoint);
+        marker = new Marker(mapView);
+        marker.setPosition(startPoint);
+        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        mapView.getOverlays().add(marker);
 
-        getChildFragmentManager().beginTransaction().replace(R.id.map, fragment).commit();
-        View root = binding.getRoot();
         return root;
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        binding = null;
+        // Remove map view
+        ViewGroup parent = (ViewGroup) mapView.getParent();
+        parent.removeView(mapView);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
     }
 }
