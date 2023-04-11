d5baa0a764c509dbed44cadb2d957dffb9e2953c
Axel Delille <85492223+Tsukoyachi@users.noreply.github.com>
2023-04-11 16:52:09 +0200

    feat(navbar) : new navbar and delete bullshit

---
+++ app/src/main/java/com/example/ihm_cabum/ui/archieve/ArchiveActivity.java
@@ -0,0 +1,15 @@
+package com.example.ihm_cabum.ui.archieve;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ihm_cabum.R;
+
+public class ArchiveActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_archive);
+    }
+}
---
+++ app/src/main/java/com/example/ihm_cabum/ui/home/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.example.ihm_cabum.ui.home;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ihm_cabum.R;
+
+public class HomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
