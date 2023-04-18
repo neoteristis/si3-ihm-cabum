4a89346207b5cea2723182768b81f27587de816e
BadWolf03 <tspam765@gmail.com>
2023-04-18 21:44:45 +0300

    adding basic customization

---
+++ app/src/main/java/com/example/ihm_cabum/Accident.java
@@ -0,0 +1,84 @@
+package com.example.ihm_cabum;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import org.osmdroid.util.GeoPoint;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Accident {
+    private final AccidentType typeOfAccident;
+    private final String description;
+    private final byte[] image;
+    private final GeoPoint address;
+    private final Date time;
+
+    private int numberOfApproval;
+
+    public Accident(AccidentType typeOfAccident, String description, byte[] image, GeoPoint address, Date time) {
+        this.typeOfAccident = typeOfAccident;
+        this.description = description;
+        this.image = image;
+        this.address = address;
+        this.time = time;
+
+        this.numberOfApproval = 0;
+    }
+
+    public Accident(AccidentType typeOfAccident, String description, byte[] image, GeoPoint address, Date time, int numberOfApproval) {
+        this.typeOfAccident = typeOfAccident;
+        this.description = description;
+        this.image = image;
+        this.address = address;
+        this.time = time;
+        this.numberOfApproval = numberOfApproval;
+    }
+
+    public AccidentType getTypeOfAccident() {
+        return typeOfAccident;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public Bitmap BitmapImage() {
+        // Convert byte array to Bitmap
+        return BitmapFactory.decodeByteArray(image, 0, image.length);
+    }
+
+    public GeoPoint getAddress() {
+        return address;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public int getNumberOfApproval() {
+        return numberOfApproval;
+    }
+
+    public void approved() {
+        this.numberOfApproval++;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Accident accident = (Accident) o;
+        return typeOfAccident == accident.typeOfAccident && Objects.equals(description, accident.description) && Objects.equals(address, accident.address) && Objects.equals(time, accident.time);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(typeOfAccident, description, address, time);
+    }
+}
--- app/src/main/java/com/example/ihm_cabum/MainActivity.java
+++ app/src/main/java/com/example/ihm_cabum/MainActivity.java
@@ -1,13 +1,10 @@
 package com.example.ihm_cabum;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.Menu;
@@ -16,7 +13,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.Button;
 import android.widget.SearchView;
 
 import com.android.volley.Request;
@@ -34,6 +30,9 @@ import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.Marker;
 
+import java.io.ByteArrayOutputStream;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
 
     private MapView mapView;
@@ -44,14 +43,15 @@ public class MainActivity extends AppCompatActivity {
 
     private boolean isAddClicked = false;
 
-    private Marker addMarker(int icon, GeoPoint location, String title, String description, int imageResource){
+    private Marker addMarker(Accident accident){
         Marker marker = new Marker(mapView);
         marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
-        marker.setIcon(getDrawable(icon));
-        marker.setPosition(location);
-        marker.setTitle(title);
-        marker.setSubDescription(description);
-        marker.setImage(getDrawable(imageResource));
+
+        marker.setIcon(getDrawable(accident.getTypeOfAccident().getIconResource()));
+        marker.setPosition(accident.getAddress());
+        marker.setTitle(accident.getTypeOfAccident().getLabel());
+        marker.setSubDescription(accident.getDescription());
+        marker.setImage(new BitmapDrawable(getResources(), accident.BitmapImage()));
         marker.setPanToView(true);  //the map will be centered on the marker position.
         marker.setDraggable(true);
         return marker;
@@ -64,28 +64,64 @@ public class MainActivity extends AppCompatActivity {
         mapController.setCenter(startPoint);
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        Animation fromBottomAnimation = AnimationUtils.loadAnimation(this, R.anim.from_bottom_anim);
-        Animation toBottomAnimation = AnimationUtils.loadAnimation(this, R.anim.to_bottom_anim);
-        Animation rotateCloseAnimation = AnimationUtils.loadAnimation(this, R.anim.rotate_close_animation);
-        Animation rotateOpenAnimation = AnimationUtils.loadAnimation(this, R.anim.rotate_open_animation);
-
-        Configuration.getInstance().load(   getApplicationContext(),  PreferenceManager.getDefaultSharedPreferences(getApplicationContext()) );
-        setContentView(R.layout.activity_home);
-
+    private void setUpMap() {
         mapView = findViewById(R.id.mapView);
         mapView.setTileSource(TileSourceFactory.MAPNIK);    //render
         mapView.setBuiltInZoomControls(true);               // zoomable
         mapView.setMultiTouchControls(true);                //  zoom with 2 fingers
+    }
 
-        setMapCenterPosition(DEFAULT_LATITUDE, DEFAULT_LONGITUDE);
+    //TODO to change for the real view
+    private void setUpAccidentsNear() {
+        //TODO tobe changed to actual data
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.work);
 
-        mapView.getOverlays().add( addMarker(org.osmdroid.library.R.drawable.marker_default, new GeoPoint(43.65120,7.00450), "fred", "my home", R.drawable.home ) );
-        mapView.getOverlays().add( addMarker(org.osmdroid.library.R.drawable.moreinfo_arrow, new GeoPoint(43.65050,7.00726), "job",  "my job", R.drawable.work ) );
-        mapView.getOverlays().add( addMarker(org.osmdroid.library.R.drawable.osm_ic_follow_me_on, new GeoPoint(43.64950,7.00418), "lisa",  "my teacher", R.drawable.work ) );
+// Convert Bitmap to byte array
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] byteArray = stream.toByteArray();
+
+        mapView.getOverlays().add(addMarker(
+                new Accident(
+                        AccidentType.COLLISION_SINGLE_VEHICLE,
+                        "some test description in order to check",
+                        byteArray,
+                        new GeoPoint(43.65050,7.00726),
+                        new Date(),
+                        20
+                )
+            )
+        );
+
+        mapView.getOverlays().add(addMarker(
+                        new Accident(
+                                AccidentType.COLLISION_SINGLE_VEHICLE,
+                                "some test description in order to check",
+                                byteArray,
+                                new GeoPoint(43.65050,7.00726),
+                                new Date(),
+                                20
+                        )
+                )
+        );
+        mapView.getOverlays().add(addMarker(
+                        new Accident(
+                                AccidentType.COLLISION_SINGLE_VEHICLE,
+                                "some test description in order to check",
+                                byteArray,
+                                new GeoPoint(43.64950,7.00418),
+                                new Date(),
+                                20
+                        )
+                )
+        );
+    }
+
+    private void setUpAddButton() {
+        Animation fromBottomAnimation = AnimationUtils.loadAnimation(this, R.anim.from_bottom_anim);
+        Animation toBottomAnimation = AnimationUtils.loadAnimation(this, R.anim.to_bottom_anim);
+        Animation rotateCloseAnimation = AnimationUtils.loadAnimation(this, R.anim.rotate_close_animation);
+        Animation rotateOpenAnimation = AnimationUtils.loadAnimation(this, R.anim.rotate_open_animation);
 
         FloatingActionButton addButton = findViewById(R.id.add_button);
         addButton.setOnClickListener(new View.OnClickListener() {
@@ -116,6 +152,18 @@ public class MainActivity extends AppCompatActivity {
                 isAddClicked = !isAddClicked;
             }
         });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Configuration.getInstance().load(getApplicationContext(),  PreferenceManager.getDefaultSharedPreferences(getApplicationContext()) );
+        setContentView(R.layout.activity_home);
+
+        setUpMap();
+        setMapCenterPosition(DEFAULT_LATITUDE, DEFAULT_LONGITUDE);
+        setUpAccidentsNear();
+        setUpAddButton();
     }
 
     @Override
@@ -163,7 +211,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //Nominatim API
-    public void convertCityNameToCoordinates(String cityName) {
+    private void convertCityNameToCoordinates(String cityName) {
         String url = "https://nominatim.openstreetmap.org/search?q=" + cityName + "&format=json";
 
         JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null,
@@ -179,6 +227,7 @@ public class MainActivity extends AppCompatActivity {
                     error.printStackTrace();
                 });
 
+        //async
         RequestQueue queue = Volley.newRequestQueue(this);
         queue.add(request);
     }
