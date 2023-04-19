d78a85e8d18148433fb93b383f4bae3f8e0c5428
Nina Boulton <nina.boulton@educlever.com>
2023-04-19 11:31:07 +0200

    merge

--- app/src/main/java/com/example/ihm_cabum/MainActivity.java
+++ app/src/main/java/com/example/ihm_cabum/MainActivity.java
@@ -2,15 +2,11 @@ package com.example.ihm_cabum;
 
 import static com.example.ihm_cabum.notification.NotificationHelper.createNotificationChannel;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
 import android.os.Bundle;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static boolean notificationsOn = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/ihm_cabum/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/ihm_cabum/ui/profile/ProfileFragment.java
@@ -1,17 +1,13 @@
 package com.example.ihm_cabum.ui.profile;
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
 import com.example.ihm_cabum.R;
 import com.example.ihm_cabum.databinding.FragmentProfileBinding;
-
 public class ProfileFragment extends Fragment {
     private FragmentProfileBinding binding;
 
@@ -20,14 +16,12 @@ public class ProfileFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_profile,container,false);
-
         binding = FragmentProfileBinding.inflate(inflater, container, false);
-        view = binding.getRoot();
+        View view = binding.getRoot();
 
         getChildFragmentManager()
                 .beginTransaction()
-                .replace(R.id.profile_page, new ProfileFragment_utils.ProfileInnerFragment(view))
+                .replace(R.id.profile_page, new ProfileInnerFragment(view))
                 .commit();
 
         return view;
