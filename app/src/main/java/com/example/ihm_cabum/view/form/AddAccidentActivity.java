9720c3dd8509c02ffdf4fa8278066429df471b31
BadWolf03 <tspam765@gmail.com>
2023-05-06 19:09:06 +0300

    finishing the functionality of add form

--- app/src/main/java/com/example/ihm_cabum/view/form/AddAccidentActivity.java
+++ app/src/main/java/com/example/ihm_cabum/view/form/AddAccidentActivity.java
@@ -1,16 +1,108 @@
 package com.example.ihm_cabum.view.form;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.ihm_cabum.R;
+import com.example.ihm_cabum.model.AccidentType;
+import com.example.ihm_cabum.model.DisasterType;
+
+import java.text.DateFormat;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.stream.IntStream;
 
 public class AddAccidentActivity extends AppCompatActivity {
+
+    private final static String[] ITEMS_DISASTER = Arrays.stream(DisasterType.values()).map(DisasterType::getLabel).toArray(String[]::new);
+    private final static String[] ITEMS_ACCIDENT_TYPE = Arrays.stream(AccidentType.values()).map(AccidentType::getLabel).toArray(String[]::new);
+
+    private final static String DEFAULT_HEADER_BEGINNING = "Create an ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_accident);
+
+        TextView header = findViewById(R.id.title_addForm);
+        Spinner spinnerDisasterType = findViewById(R.id.accident_incident_menu_addForm);
+        Spinner spinnerAccidentType = findViewById(R.id.type_menu_addForm);
+
+        CalendarView calendar = findViewById(R.id.calendar_addForm);
+        ImageButton calendarButton = findViewById(R.id.callendar_button_addForm);
+        DateFormat dateFormat = android.text.format.DateFormat.getDateFormat(getApplicationContext());
+
+        EditText dateField = findViewById(R.id.date_addForm);
+        EditText timeField = findViewById(R.id.time_addForm);
+
+        Button cancelButton = findViewById(R.id.cancel_button_addFrom);
+        Button saveButton = findViewById(R.id.add_button_addForm);
+
+        spinnerDisasterType.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, ITEMS_DISASTER));
+        spinnerAccidentType.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, ITEMS_ACCIDENT_TYPE));
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String selected = extras.getString("type");
+            header.setText(DEFAULT_HEADER_BEGINNING + selected);
+
+            spinnerDisasterType.setSelection(
+                    IntStream.range(0, ITEMS_DISASTER.length)
+                            .filter(i -> ITEMS_DISASTER[i].toLowerCase().equals(selected))
+                            .findFirst()
+                            .orElse(0)
+            );
+        }
+        calendarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calendar.setVisibility(calendar.getVisibility() == View.INVISIBLE ? View.VISIBLE : View.INVISIBLE);
+            }
+        });
+
+        spinnerDisasterType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                header.setText(DEFAULT_HEADER_BEGINNING + adapterView.getItemAtPosition(i).toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        calendar.setVisibility(View.INVISIBLE);
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
+                dateField.setText(dateFormat.format(new Date(year, month, day)));
+                calendar.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        dateField.setText(dateFormat.format(new Date()));
+        Calendar calendar1 = Calendar.getInstance();
+        timeField.setText(calendar1.get(Calendar.HOUR_OF_DAY) + ":" + calendar1.get(Calendar.MINUTE));
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/ihm_cabum/view/reusable/AddButtonsFragment.java
+++ app/src/main/java/com/example/ihm_cabum/view/reusable/AddButtonsFragment.java
@@ -31,11 +31,6 @@ public class AddButtonsFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_add_buttons, container, false);
 
-        Animation fromBottomAnimation = AnimationUtils.loadAnimation(getContext(), R.anim.from_bottom_anim);
-        Animation toBottomAnimation = AnimationUtils.loadAnimation(getContext(), R.anim.to_bottom_anim);
-        Animation rotateCloseAnimation = AnimationUtils.loadAnimation(getContext(), R.anim.rotate_close_animation);
-        Animation rotateOpenAnimation = AnimationUtils.loadAnimation(getContext(), R.anim.rotate_open_animation);
-
         FloatingActionButton addButton = view.findViewById(R.id.add_button);
         FloatingActionButton accident = view.findViewById(R.id.accident);
         FloatingActionButton incident = view.findViewById(R.id.incident);
@@ -43,48 +38,73 @@ public class AddButtonsFragment extends Fragment {
         if(addButton == null || accident == null || incident == null) {
             return view;
         }
+        ConstraintLayout constraintLayout = view.findViewById(R.id.tabAdd);
 
         accident.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                changeVisibility(addButton, accident, incident, constraintLayout);
                 Intent intent = new Intent(getActivity(), AddAccidentActivity.class);
+                intent.putExtra("type", "accident");
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(intent);
             }
         });
 
-        ConstraintLayout constraintLayout = view.findViewById(R.id.tabAdd);
+        incident.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeVisibility(addButton, accident, incident, constraintLayout);
+                Intent intent = new Intent(getActivity(), AddAccidentActivity.class);
+                intent.putExtra("type", "incident");
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
 
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (isAddClicked) {
-                    accident.setVisibility(View.INVISIBLE);
-                    incident.setVisibility(View.INVISIBLE);
-
-                    constraintLayout.setVisibility(View.INVISIBLE);
-
-                    accident.startAnimation(toBottomAnimation);
-                    incident.startAnimation(toBottomAnimation);
-
-                    addButton.startAnimation(rotateCloseAnimation);
-                } else {
-                    constraintLayout.setVisibility(View.VISIBLE);
-
-                    accident.setVisibility(View.VISIBLE);
-                    incident.setVisibility(View.VISIBLE);
-
-                    accident.startAnimation(fromBottomAnimation);
-                    incident.startAnimation(fromBottomAnimation);
-
-                    addButton.startAnimation(rotateOpenAnimation);
-                }
-                isAddClicked = !isAddClicked;
+                changeVisibility(addButton, accident, incident, constraintLayout);
             }
         });
 
         constraintLayout.setVisibility(View.INVISIBLE);
 
         return view;
+    }
+    private void changeVisibility(
+            FloatingActionButton addButton,
+            FloatingActionButton accident,
+            FloatingActionButton incident,
+            ConstraintLayout constraintLayout) {
+
+        Animation fromBottomAnimation = AnimationUtils.loadAnimation(getContext(), R.anim.from_bottom_anim);
+        Animation toBottomAnimation = AnimationUtils.loadAnimation(getContext(), R.anim.to_bottom_anim);
+        Animation rotateCloseAnimation = AnimationUtils.loadAnimation(getContext(), R.anim.rotate_close_animation);
+        Animation rotateOpenAnimation = AnimationUtils.loadAnimation(getContext(), R.anim.rotate_open_animation);
+
+        if (isAddClicked) {
+            accident.setVisibility(View.INVISIBLE);
+            incident.setVisibility(View.INVISIBLE);
+
+            constraintLayout.setVisibility(View.INVISIBLE);
+
+            accident.startAnimation(toBottomAnimation);
+            incident.startAnimation(toBottomAnimation);
+
+            addButton.startAnimation(rotateCloseAnimation);
+        } else {
+            constraintLayout.setVisibility(View.VISIBLE);
+
+            accident.setVisibility(View.VISIBLE);
+            incident.setVisibility(View.VISIBLE);
+
+            accident.startAnimation(fromBottomAnimation);
+            incident.startAnimation(fromBottomAnimation);
+
+            addButton.startAnimation(rotateOpenAnimation);
+        }
+        isAddClicked = !isAddClicked;
     }
 }
