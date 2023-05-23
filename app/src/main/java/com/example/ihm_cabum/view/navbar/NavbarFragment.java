beca8b009f4e7a5846eb51bd96a752136ff77464
BadWolf03 <tspam765@gmail.com>
2023-05-23 16:11:13 +0300

    finish animation, add load activity

--- app/src/main/java/com/example/ihm_cabum/view/navbar/NavbarFragment.java
+++ app/src/main/java/com/example/ihm_cabum/view/navbar/NavbarFragment.java
@@ -10,7 +10,9 @@ import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
+import com.example.ihm_cabum.view.form.AddAccidentActivity;
 import com.example.ihm_cabum.view.home.HomeActivity;
 import com.example.ihm_cabum.R;
 import com.example.ihm_cabum.view.archieve.ArchiveActivity;
@@ -24,6 +26,7 @@ public class NavbarFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_navbar,container,false);
 
+        FragmentActivity activity = getActivity();
         Button homeBtn = view.findViewById(R.id.navigation_home);
         homeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,7 +35,11 @@ public class NavbarFragment extends Fragment {
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(intent);
 
-                getActivity().overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                if(activity instanceof AddAccidentActivity || activity instanceof HomeActivity) {
+                    getActivity().overridePendingTransition(R.anim.slide_in_bottom, R.anim.slide_out_top);
+                } else {
+                    getActivity().overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+                }
             }
         });
         Button archiveBtn = view.findViewById(R.id.navigation_archive);
@@ -43,7 +50,13 @@ public class NavbarFragment extends Fragment {
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(intent);
 
-                getActivity().overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                if(activity instanceof ProfileActivity) {
+                    getActivity().overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+                } else if(activity instanceof HomeActivity) {
+                    getActivity().overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                } else {
+                    getActivity().overridePendingTransition(R.anim.slide_in_bottom, R.anim.slide_out_top);
+                }
             }
         });
         Button profileBtn = view.findViewById(R.id.navigation_profile);
@@ -54,7 +67,11 @@ public class NavbarFragment extends Fragment {
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(intent);
 
-                getActivity().overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                if(activity instanceof AddAccidentActivity || activity instanceof ProfileActivity) {
+                    getActivity().overridePendingTransition(R.anim.slide_in_bottom, R.anim.slide_out_top);
+                } else {
+                    getActivity().overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                }
             }
         });
 
